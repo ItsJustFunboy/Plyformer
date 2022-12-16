4d8aa38868cf42a0e3192ff9359f6bf6478c5040
ItsJustFunboy <naumchikartem2304@gmail.com>
2022-12-16 23:51:35 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import tkinter
+import os
+
+# Data
+
+WINDOW_HEIGHT = 64*8
+WINDOW_WIDTH = 64*12
+
+WINDOW_NAME = "Plyformer"
+
+ASSETS_FOLDER = "Assets\\"
+
+# Images
+
+#IMAGES_GROUND_FILL = f'{ASSETS_FOLDER}'
+#TODO: Import images, and create the main scene
+root = tkinter.Tk()
+
+
+
+root.mainloop()
